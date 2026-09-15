c7c31bd6b4a735b437c9236397a64ba8e7f0ccdb
Minh Nguyen <nguyengiaminh1910@gmail.com>
2026-09-15 18:10:09 +0700

    Video28

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import './App.css';
 import { useState } from 'react';
-import Calculator from './Calculator';
+import logo from './logo.svg';
 
+const Mycomponent = () => {
+  return(
+    <div> hỏi dân IT update</div>
+  );
+}
 const App = () => {
   const [count, setCount] = useState(0);
-//   function myFunction() {
-//     console.log(">>> run my function")
-// }
-  const myFunction = () => {
-    console.log(">>> run my arrow function")
-  }
-myFunction()
   return (
     <div className="App">
-      <h1>Calculator</h1>
-      <Calculator />
-    </div>  
-  )
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Mycomponent/>
+      </header>
+    </div>
+  );
 }
-
 export default App;
-
