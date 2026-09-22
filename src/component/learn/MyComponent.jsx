4730cbeb5af14e4e449415b28881ae899650d2e6
Minh Nguyen <nguyengiaminh1910@gmail.com>
2026-09-22 21:41:37 +0700

    Video 30

--- src/component/learn/MyComponent.jsx
+++ src/component/learn/MyComponent.jsx
@@ -1,6 +1,12 @@
+import './style.css'
 const MyComponent = () => {
   return(
-    <div> hỏi dân IT update</div>
+    <>
+      <div> hỏi dân IT update</div> 
+      <div className = "child"
+        style = {{borderRadius: "10px"}}
+      >child</div>
+    </>
   );
 }
 export default MyComponent;
