c0bb19c9c291a269e56fa13688d99af3b38327c2
Minh Nguyen <nguyengiaminh1910@gmail.com>
2026-09-24 21:58:30 +0700

    Video 31

--- src/component/learn/MyComponent.jsx
+++ src/component/learn/MyComponent.jsx
@@ -1,8 +1,19 @@
 import './style.css'
 const MyComponent = () => {
+  // const hoidanit = "eric"; //string
+  // const hoidanit = 23 // number
+  // const hoidanit = true // boolean
+  // const hoidanit = undefined
+  // const hoidanit = null
+  const hoidanit = [1,2,3]
+  // const hoidanit = {
+  //   name: "hoidanit",
+  //   age: 20
+  // }
   return(
     <>
-      <div> hỏi dân IT update</div> 
+      <div> {JSON.stringify(hoidanit)} và hỏi dân IT update</div>
+      <div>{console.log("Minh")}</div> 
       <div className = "child"
         style = {{borderRadius: "10px"}}
       >child</div>
